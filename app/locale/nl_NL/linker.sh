fa5ce8a5a5c3522bd91210166646d2053b81d03d
Rob Jentzema <rob@wellnessbon.nl>
2014-11-23 13:49:32 +0100

    small script update

--- app/locale/nl_NL/linker.sh
+++ app/locale/nl_NL/linker.sh
@@ -1,10 +1,24 @@
 #!/usr/bin/env sh
 
+# The use of symlinks under windows operating systems is problematic
+# and requires some technical knowledge and persistence to resolve
+# this issue in a sufficient matter. Hence publishing these files
+# as symlinks is not very portable. For now, it does facilitate safe
+# and easier organization of the files in terms of translation prio
+# of files (first core, then most often used? to discuss with Ho_nl)
+# Hence we provide the command to erase all symlinks and copy all files
+my=./$0
 declare -a csva=($(find lib -type f -name \*.csv))
 
 case "$1" in
 
-    (c|create|mk|make)
+    (c|copy)
+        $my ls      
+        #find . -type l -mtime -0 -exec mv -t . {} +
+
+        ;;
+
+    (mk|make)
 
         for f in ${csva[@]}; do
             test -h $(basename $f) || ln -s $f $(basename $f)
@@ -18,7 +32,7 @@ case "$1" in
 
     (l|ls|list)
 
-        find . -type l -printf '%p -> %l\n'
+        find . -type l -printf 'moved %l → %p\n'
         ;;
 
     (v|val|validate)
